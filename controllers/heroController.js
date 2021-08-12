c657ffcceeae4a7453b3fb213600c6259763baba
Pedro Enciso <penciso@ucsd.edu>
2021-08-12 15:54:17 -0700

    handle POST from /heroes/add_hero

--- controllers/heroController.js
+++ controllers/heroController.js
@@ -1,5 +1,7 @@
 const Hero = require("../models/hero");
 const async = require("async");
+const { body, validationResult } = require("express-validator");
+const hero_image = require("../public/images/default-hero.png");
 
 // display list of all heroes on home page
 exports.index = function (req, res) {
@@ -37,7 +39,99 @@ exports.hero_detail = function (req, res, next) {
   );
 };
 
-// display form to add a new hero
+// display form to add a new hero on GET
 exports.add_hero = function (req, res) {
   res.render("add_hero", { title: "Create a New Hero" });
 };
+
+// handle hero create on post
+exports.hero_create_post = [
+  // validate and sanitize fields
+  body("hero_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("New hero must have a name")
+    .isAlphanumeric()
+    .withMessage("Hero name contains non-alphanumeric characters."),
+  body("hero_identity")
+    .optional({ checkFalsy: true })
+    .isAlphanumeric()
+    .withMessage("Hero identity contains non-alphanumeric characters."),
+  body("alignment", "You must choose an alignment")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body(
+    "intelligence",
+    "Rating must be between a number between 1 and 100"
+  ).isInt({ min: 1, max: 100 }),
+  body("strength", "Rating must be between a number between 1 and 100").isInt({
+    min: 1,
+    max: 100,
+  }),
+  body("speed", "Rating must be between a number between 1 and 100").isInt({
+    min: 1,
+    max: 100,
+  }),
+  body(
+    "durability",
+    "Rating must be between a number between 1 and 100"
+  ).isInt({ min: 1, max: 100 }),
+  body("power", "Rating must be between a number between 1 and 100").isInt({
+    min: 1,
+    max: 100,
+  }),
+  body("combat", "Rating must be between a number between 1 and 100").isInt({
+    min: 1,
+    max: 100,
+  }),
+  body("publisher")
+    .optional({ checkFalsy: true })
+    .isAlphanumeric()
+    .withMessage("Publisher contains non-alphanumeric characters."),
+  body("first_appearance").optional({ checkFalsy: true }),
+
+  // process request after validation/sanitization
+  (req, res, next) => {
+    // extract validation errors from a request
+    const errors = validationResults(req);
+
+    // create powerstats object from request to feed to new hero
+    const userStats = {
+      intelligence: req.body.intelligence,
+      strength: req.body.strength,
+      speed: req.body.speed,
+      durability: req.body.durability,
+      power: req.body.power,
+      combat: req.body.combat,
+    };
+
+    // create a Hero object with escaped and trimmed data
+    const hero = new Hero({
+      name: req.body.hero_name,
+      identity: req.body.hero_identity,
+      alignment: req.body.alignment,
+      image: hero_image,
+      publisher: req.body.publisher,
+      first_appearence: req.body.first_appearance,
+      powerstats: userStats,
+    });
+
+    if (!errors.isEmpty()) {
+      // there are errors, render form again with error messages
+      res.render("add_hero", {
+        title: "Create a New Hero",
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // data from form is valid
+      hero.save((err) => {
+        if (err) return next(err);
+      });
+      // successfully saved hero to database, so render new hero page
+      res.redirect(hero.url);
+    }
+  },
+];
--- routes/heroes.js
+++ routes/heroes.js
@@ -8,6 +8,9 @@ router.get("/", hero_controller.index);
 // Get add hero form
 router.get("/add_hero", hero_controller.add_hero);
 
+// POST request to create a hero
+router.post("/add_hero", hero_controller.hero_create_post);
+
 // GET hero detail page
 router.get("/:id", hero_controller.hero_detail);
 
